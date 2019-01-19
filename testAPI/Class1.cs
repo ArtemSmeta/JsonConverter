eb609abb0a4ce6669bb27994edb21c74f496a49b
Artem <smetatsc@yandex.ru>
2019-01-19 12:02:07 +0300

    qq

---
+++ testAPI/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testAPI
+{
+    public class Class1
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public IEnumerable<Person> Cases { get; set; }
+    }
+}
